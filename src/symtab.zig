68a2995408f4d729f949e1ec3665a00d4c15b727
Jean Lima Andrade <jyeno@paranoid.email>
2021-10-28 07:49:24 -0300

    symtab: improve API, use ArenaAllocator

--- src/symtab.zig
+++ src/symtab.zig
@@ -3,38 +3,37 @@ const std = @import("std");
 
 // TODO consider if should be a pointer
 pub const ValType = union(enum) {
+    // should be the owner of memory
     str: []const u8,
     // TODO parser word array
     array: [][]const u8,
-    // alias: [][]const u8,
     // TODO function
 };
 
 /// Symbol Table multi-level representation, internally it uses a hash map.
 pub const SymTab = struct {
-    allocator: *std.mem.Allocator,
-    parent: ?*SymTab,
-    table: HashMap,
+    arena: std.heap.ArenaAllocator,
+    parent: ?*SymTab = null,
+    table: HashMap = .{},
 
     pub const HashMap = std.StringHashMapUnmanaged(ValType);
     pub const Iterator = HashMap.Iterator;
 
     /// Initializes a symbol table
     pub fn init(allocator: *std.mem.Allocator, parent: ?*SymTab) SymTab {
-        return SymTab{ .allocator = allocator, .parent = parent, .table = .{} };
+        return SymTab{ .arena = std.heap.ArenaAllocator.init(allocator), .parent = parent };
     }
 
     pub fn initCapacity(allocator: *std.mem.Allocator, parent: ?*SymTab, capacity: u32) !SymTab {
+        var arena_allocator = std.heap.ArenaAllocator.init(allocator);
         var table: HashMap = .{};
-        try table.ensureTotalCapacity(allocator, capacity);
-        return SymTab{ .allocator = allocator, .parent = parent, .table = table };
+        try table.ensureTotalCapacity(&arena_allocator.allocator, capacity);
+        return SymTab{ .arena = arena_allocator, .parent = parent, .table = table };
     }
 
     /// Deinitializes the symbol table and its members
     pub fn deinit(self: *SymTab) void {
-        // TODO consider resources deinitialization
-        self.table.deinit(self.allocator);
-        self.allocator.destroy(self);
+        self.arena.deinit();
     }
 
     /// Merges the symbol table with its parent
@@ -43,15 +42,14 @@ pub const SymTab = struct {
             parent.table.ensureUnusedCapacity(parent.allocator, self.table.capacity);
             var it = self.table.iterator();
             while (it.next()) |item| {
-                // TODO analize what happens when memory is free'd
-                parent.putAssumeCapacity(item.key_ptr, item.value_ptr.*);
+                parent.putAssumeCapacityCopyVal(item.key_ptr, item.value_ptr.*);
             }
         }
     }
 
     /// Do a local search based on given key, returns the value of
     /// given key or null in failure
-    pub fn localLookup(self: *SymTab, key: []const u8) ?ValType {
+    pub fn lookup(self: *SymTab, key: []const u8) ?ValType {
         return self.table.get(key);
     }
 
@@ -59,30 +57,49 @@ pub const SymTab = struct {
     /// if there is no match, search on the top-level symbol tables
     /// return the value of given key or null in failure.
     pub fn globalLookup(self: *SymTab, key: []const u8) ?ValType {
-        if (self.local_lookup(key)) |value| {
+        if (self.lookup(key)) |value| {
             return value;
         } else if (self.parent) |parent| {
-            return parent.global_lookup(key);
+            return parent.globalLookup(key);
         } else {
             return null;
         }
     }
 
-    /// Inserts `value` with given `key`, if there is already an entry, it is overwrited
+    /// Inserts `value` with given `key`, if there is already an entry, it is overwriten
     pub fn put(self: *SymTab, key: []const u8, value: ValType) !void {
-        // TODO maybe a replace function, and it would require no allocation inside, so safe
-        try self.table.put(self.allocator, key, value);
+        try self.table.put(&self.arena.allocator, key, value);
     }
 
-    /// Inserts `value` with given `key`, if there is already an entry, it is overwrited
+    /// Inserts `value` with given `key`, if there is already an entry, it is overwriten
+    /// Copies the data of `value`
+    pub fn putCopyVal(self: *SymTab, key: []const u8, value: ValType) !void {
+        const new_value = .{ .str = try std.mem.dupe(&self.arena.allocator, u8, value.str) };
+        try self.table.put(&self.arena.allocator, key, new_value);
+    }
+
+    /// Inserts `value` with given `key`, if there is already an entry, it is overwriten
     pub fn putAssumeCapacity(self: *SymTab, key: []const u8, value: ValType) void {
-        // TODO maybe a replace function, and it would require no allocation inside, so safe
         self.table.putAssumeCapacity(key, value);
+    }
+
+    /// Inserts `value` with given `key`, if there is already an entry, it is overwriten
+    /// Copies the data of `value`
+    pub fn putAssumeCapacityCopyVal(self: *SymTab, key: []const u8, value: ValType) !void {
+        // TODO maybe a replace function, and it would require no allocation inside, so safe
+        const new_value = .{ .str = try std.mem.dupe(&self.arena.allocator, u8, value.str) };
+        self.table.putAssumeCapacity(self.allocator, key, new_value);
     }
 
     /// Remove entry with given `key`, and returns it if exists.
     pub fn pop(self: *SymTab, key: []const u8) ValType {
         return self.table.remove(key);
+    }
+
+    /// Remove entry with given `key`, and also deallocate the memory allocated
+    pub fn del(self: *SymTab, key: []const u8) void {
+        const value = self.table.remove(key);
+        self.arena.allocator.free(value.?.str);
     }
 
     /// Returns the table iterator.
@@ -98,6 +115,7 @@ pub const SymTab = struct {
     pub fn dupeZ(self: *SymTab, allocator: *std.mem.Allocator) ![*:null]?[*:0]const u8 {
         var array = try std.ArrayList(?[*:0]const u8).initCapacity(allocator, self.table.count() + 1);
         defer array.deinit();
+        // TODO analize possibility of get parent items too
 
         var it = self.table.iterator();
         while (it.next()) |entry| {
@@ -109,12 +127,11 @@ pub const SymTab = struct {
 };
 
 // this variable is populated at the main
-pub var global_symtab: *SymTab = undefined;
+pub var global_symtab: SymTab = undefined;
 
 /// Initializes the global symbol table, with the contents std.os.environ
 pub fn initGlobalSymbolTable(allocator: *std.mem.Allocator) !void {
-    global_symtab = try allocator.create(SymTab);
-    global_symtab.* = try SymTab.initCapacity(allocator, null, @intCast(u32, std.os.environ.len));
+    global_symtab = try SymTab.initCapacity(allocator, null, @intCast(u32, std.os.environ.len));
     for (std.os.environ) |env| {
         const equalsIdx: ?usize = blk: {
             var index: usize = 0;
@@ -134,5 +151,3 @@ pub fn initGlobalSymbolTable(allocator: *std.mem.Allocator) !void {
 }
 
 test "Symbol Table" {}
-
-test "Symbol Global Table" {}
